02500f5d5ec30db223188c68628180a4180a60a9
Stuart Williams (ETS) <stuwilliams@corelogic.com>
2019-10-10 15:40:31 -0700

    NuGet Package

--- SecureRandom_Test/GlobalSuppressions.cs
+++ SecureRandom_Test/GlobalSuppressions.cs
@@ -1,4 +1,9 @@
-﻿
+﻿[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SCS0005:Weak random generator", Justification = "<Pending>", Scope = "member", Target = "~M:SecureRandom_Test.SystemRandom_Tests.Random_Next_Bytes")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SCS0005:Weak random generator", Justification = "<Pending>", Scope = "member", Target = "~M:SecureRandom_Test.SystemRandom_Tests.Random_Next_Bytes_Span")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SCS0005:Weak random generator", Justification = "<Pending>", Scope = "member", Target = "~M:SecureRandom_Test.SystemRandom_Tests.Random_Next_Double")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SCS0005:Weak random generator", Justification = "<Pending>", Scope = "member", Target = "~M:SecureRandom_Test.SystemRandom_Tests.Ransom_Next_0")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SCS0005:Weak random generator", Justification = "<Pending>", Scope = "member", Target = "~M:SecureRandom_Test.SystemRandom_Tests.Ransom_Next_Max")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "SCS0005:Weak random generator", Justification = "<Pending>", Scope = "member", Target = "~M:SecureRandom_Test.SystemRandom_Tests.Ransom_Next_Min_Max")]
 // This file is used by Code Analysis to maintain SuppressMessage 
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given 
